e0c55e0701bbec95b9dd7f67007241f66280e978
Bohdan Pasieka <bohdan_pasieka@epam.com>
2022-07-17 14:13:38 +0300

    navigation-bar

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,40 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import { AllUsers, Auth, Profile, UserDetails } from './pages'
+// Components
+import { NavigationBar } from './components'
+// Styles
+import 'normalize.css';
 
 const App = () => {
 
   const isAuth = true; // Todo: replace when data is received
 
   return (
-    <Routes>
-      {
-        isAuth ? <>
-          <Route path='/' element={<Profile />} />
-          <Route path='/all-users' element={<AllUsers />} />
-          <Route path='/users-details/:id' element={<UserDetails />} />
-          <Route
-            path="*"
-            element={<Profile />}
-          />
-        </> :
-          <>
-            <Route path='/' exact element={<Auth />} />
+    <>
+      <NavigationBar isAuth={isAuth}/>
+      <Routes>
+        {
+          isAuth ? <>
+            <Route path='/' element={<Profile />} />
+            <Route path='/all-users' element={<AllUsers />} />
+            <Route path='/users-details/:id' element={<UserDetails />} />
             <Route
               path="*"
-              element={<Auth />}
+              element={<Profile />}
             />
-          </>
-      }
+          </> :
+            <>
+              <Route path='/' exact element={<Auth />} />
+              <Route
+                path="*"
+                element={<Auth />}
+              />
+            </>
+        }
 
-    </Routes >
+      </Routes >
+    </>
   );
 }
 
--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { Box, AppBar, Toolbar, Typography } from '@mui/material'
+import { NavLink } from 'react-router-dom';
+import LogoutIcon from '@mui/icons-material/Logout';
+// Styles
+import './navigation-bar.css'
 
-const NavigationBar = () => {
+const NavigationBar = ({ isAuth }) => {
   return (
-    <div>NavigationBar</div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        {
+          !isAuth ?
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <NavLink to='/' className='nav-link'>User Viewer</NavLink>
+              </Typography>
+            </Toolbar>
+            :
+            <Toolbar sx={{ alignItems: 'center' }}>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <NavLink to='/' className='nav-link'>User Viewer</NavLink>
+              </Typography>
+
+              <Typography variant="h6" component="div" sx={{ justifySelf: 'end', mr: 3 }}>
+                <NavLink to='/' className='nav-link'>Profile</NavLink>
+              </Typography>
+
+              <Typography variant="h6" component="div" sx={{ justifySelf: 'end', mr: 3 }}>
+                <NavLink to='/all-users' className='nav-link'>All users</NavLink>
+              </Typography>
+
+              <div className="flex-inline">
+                <Typography variant="h6" component="div" sx={{ justifySelf: 'end' }}>
+                  Logout
+                </Typography>
+
+                <LogoutIcon sx={{ ml: 1 }} />
+              </div>
+            </Toolbar>
+        }
+      </AppBar>
+    </Box>
   )
 }
 
